2b5de26e1645c85aa8dc073d036044a3026c8509
Evgeny Safronov <division494@gmail.com>
2015-04-08 01:58:39 +0300

    [Doc] More docs.

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -185,7 +185,9 @@ pub type Result<T> = result::Result<T, Error>;
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum Integer {
+    /// Every non-negative integer is treated as u64, even if it fits in i64.
     U64(u64),
+    /// Every negative integer is treated as i64.
     I64(i64),
 }
 
