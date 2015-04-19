0f265cdea442de23da0b945a88cb4dbbbb2f090e
Evgeny Safronov <division494@gmail.com>
2015-04-19 21:39:52 +0300

    [Misc] Dead code elimination (already moved).

--- tests/func/unsorted.rs
+++ tests/func/unsorted.rs
@@ -33,35 +33,6 @@ fn from_bin32_max_read_len() {
 }
 
 #[test]
-fn from_nil() {
-    let buf: &[u8] = &[0xc0];
-    let mut cur = Cursor::new(buf);
-
-    assert_eq!((), read_nil(&mut cur).unwrap());
-    assert_eq!(1, cur.position());
-}
-
-#[test]
-fn from_nil_invalid_marker() {
-    let buf: &[u8] = &[0xc1];
-    let mut cur = Cursor::new(buf);
-
-    assert_eq!(Error::TypeMismatch(Marker::Reserved),
-        read_nil(&mut cur).err().unwrap());
-    assert_eq!(1, cur.position());
-}
-
-#[test]
-fn from_nil_invalid_marker_read() {
-    let buf: &[u8] = &[];
-    let mut cur = Cursor::new(buf);
-
-    assert_eq!(Error::InvalidMarkerRead(ReadError::UnexpectedEOF),
-        read_nil(&mut cur).err().unwrap());
-    assert_eq!(0, cur.position());
-}
-
-#[test]
 fn from_bool_false() {
     let buf: &[u8] = &[0xc2];
     let mut cur = Cursor::new(buf);
