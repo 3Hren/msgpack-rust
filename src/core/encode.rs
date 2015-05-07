40cd838b2a3b9b209deb5dcc959940a13668c446
Evgeny Safronov <division494@gmail.com>
2015-05-07 19:00:07 +0300

    [CC] Using standard library more wide.

--- src/core/encode.rs
+++ src/core/encode.rs
@@ -524,13 +524,13 @@ pub fn write_str_len<W>(wr: &mut W, len: u32) -> Result<Marker, ValueWriteError>
         Ok(marker)
     } else if len < 256 {
         try!(write_marker(wr, Marker::Str8));
-        write_data_u8(wr, len as u8).map(|_| Marker::Str8)
+        write_data_u8(wr, len as u8).and(Ok(Marker::Str8))
     } else if len < 65536 {
         try!(write_marker(wr, Marker::Str16));
-        write_data_u16(wr, len as u16).map(|_| Marker::Str16)
+        write_data_u16(wr, len as u16).and(Ok(Marker::Str16))
     } else {
         try!(write_marker(wr, Marker::Str32));
-        write_data_u32(wr, len).map(|_| Marker::Str32)
+        write_data_u32(wr, len).and(Ok(Marker::Str32))
     }
 }
 
@@ -549,13 +549,13 @@ pub fn write_bin_len<W>(wr: &mut W, len: u32) -> Result<Marker, ValueWriteError>
 {
     if len < 256 {
         try!(write_marker(wr, Marker::Bin8));
-        write_data_u8(wr, len as u8).map(|_| Marker::Bin8)
+        write_data_u8(wr, len as u8).and(Ok(Marker::Bin8))
     } else if len < 65536 {
         try!(write_marker(wr, Marker::Bin16));
-        write_data_u16(wr, len as u16).map(|_| Marker::Bin16)
+        write_data_u16(wr, len as u16).and(Ok(Marker::Bin16))
     } else {
         try!(write_marker(wr, Marker::Bin32));
-        write_data_u32(wr, len).map(|_| Marker::Bin32)
+        write_data_u32(wr, len).and(Ok(Marker::Bin32))
     }
 }
 
