338ab27ae04c2bc14132b283d896ebe1c24fd040
Evgeny Safronov <division494@gmail.com>
2015-05-13 23:24:23 +0300

    [Value] Defined value type close to the spec.

--- src/value.rs
+++ src/value.rs
@@ -7,9 +7,30 @@ pub enum Integer {
 }
 
 #[derive(Clone, Debug, PartialEq)]
+pub enum Float {
+    F32(f32),
+    F64(f64),
+}
+
+#[derive(Clone, Debug, PartialEq)]
 pub enum Value {
-    Null,
+    /// Nil represents nil.
+    Nil,
+    /// Boolean represents true or false.
+    Boolean(bool),
+    /// Integer represents an integer.
     Integer(Integer),
+    /// Float represents a floating point number.
+    Float(Float),
+    /// String extending Raw type represents a UTF-8 string.
     String(String),
+    /// Binary extending Raw type represents a byte array.
+    Binary(Vec<u8>),
+    /// Array represents a sequence of objects.
     Array(Vec<Value>),
+    /// Map represents key-value pairs of objects.
+    Map(Vec<(Value, Value)>),
+    /// Extended implements Extension interface: represents a tuple of type information and a byte
+    /// array where type information is an integer whose meaning is defined by applications
+    Ext(i8, Vec<u8>),
 }
