52f867912f2401b0096708ca0381975fb1700ec7
Evgeny Safronov <division494@gmail.com>
2015-06-19 00:40:47 +0300

    [Doc] Decoding part for Readme.
    This commit should resolve #12 issue.

--- src/lib.rs
+++ src/lib.rs
@@ -62,6 +62,26 @@
 //! }
 //! ```
 //!
+//! Now we have an encoded buffer, which we can decode the same way:
+//!
+//! ```rust
+//! extern crate rmp as msgpack;
+//! extern crate rustc_serialize;
+//!
+//! use rustc_serialize::Decodable;
+//! use msgpack::Decoder;
+//!
+//! fn main() {
+//!     let buf = [0x92, 0x2a, 0xaa, 0x74, 0x68, 0x65, 0x20, 0x41, 0x6e, 0x73, 0x77, 0x65, 0x72];
+//!
+//!     let mut decoder = Decoder::new(&buf[..]);
+//!
+//!     let res: (u8, String) = Decodable::decode(&mut decoder).unwrap();
+//!
+//!     assert_eq!((42u8, "the Answer".to_string()), res);
+//! }
+//! ```
+//!
 //! RMP also allows to automatically serialize/deserialize custom structures using rustc_serialize
 //! reflection. To enable this feature, derive RustcEncodable and RustcDecodable attributes as
 //! shown in the following example:
@@ -70,10 +90,10 @@
 //! extern crate rmp as msgpack;
 //! extern crate rustc_serialize;
 //!
-//! use rustc_serialize::Encodable;
-//! use msgpack::Encoder;
+//! use rustc_serialize::{Encodable, Decodable};
+//! use msgpack::{Encoder, Decoder};
 //!
-//! #[derive(RustcEncodable)]
+//! #[derive(RustcEncodable, RustcDecodable, PartialEq, Debug)]
 //! struct Custom {
 //!     id: u32,
 //!     key: String,
@@ -87,6 +107,12 @@
 //!     val.encode(&mut Encoder::new(&mut &mut buf[..]));
 //!
 //!     assert_eq!([0x92, 0x2a, 0xaa, 0x74, 0x68, 0x65, 0x20, 0x41, 0x6e, 0x73, 0x77, 0x65, 0x72], buf);
+//!
+//!     // Now try to unpack the buffer into the initial struct.
+//!     let mut decoder = Decoder::new(&buf[..]);
+//!     let res: Custom = Decodable::decode(&mut decoder).ok().unwrap();
+//!
+//!     assert_eq!(val, res);
 //! }
 //! ```
 //!
