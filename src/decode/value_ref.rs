a8e480f148fb942de87fa5a7b5952d380f17830b
Evgeny Safronov <division494@gmail.com>
2015-07-13 23:14:59 +0300

    [ValueRef] Planning.

--- src/decode/value_ref.rs
+++ src/decode/value_ref.rs
@@ -12,12 +12,16 @@ pub struct Error;
 pub fn read_value_ref<R>(rd: &mut R) -> Result<ValueRef, Error>
     where R: BufRead
 {
-    // Reading the marker involves either 1 byte read or nothing.
-    let val = match read_marker(rd).unwrap() {
+    let mut buf = rd.fill_buf().unwrap(); // TODO: May fail.
+
+    // Reading the marker involves either 1 byte read or nothing. Consumes 1 byte from `buf`, not
+    // from `rd`.
+    let val = match read_marker(&mut buf).unwrap() { // TODO: May fail (IO).
         Marker::Str8 => {
-            let len = read_numeric_data::<R, u8>(rd).unwrap() as usize;
-            let buf = rd.fill_buf().unwrap();
-            let res = from_utf8(&buf[..len]).unwrap();
+            let len = read_numeric_data::<&[u8], u8>(&mut buf).unwrap(); // TODO: May fail (IO).
+            let len = len as usize; // TODO: May panic.
+            // TODO: Check buffer length.
+            let res = from_utf8(&buf[..len]).unwrap(); // TODO: May fail (not UTF-8), return &[u8] otherwise.
             ValueRef::String(res)
         }
         _ => unimplemented!(),
