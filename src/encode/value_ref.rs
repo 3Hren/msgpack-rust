defa516a69fe62d9e6453d39f10453b623a2782d
Evgeny Safronov <division494@gmail.com>
2015-07-26 20:32:36 +0300

    ValueRef: writing nil value.

--- src/encode/value_ref.rs
+++ src/encode/value_ref.rs
@@ -1,1 +1,29 @@
 //! This module is UNSTABLE, the reason is - recently added.
+
+use std::convert::From;
+use std::io::Write;
+
+use super::super::value::ValueRef;
+use super::*;
+
+#[derive(Debug)]
+pub struct Error(WriteError);
+
+impl From<FixedValueWriteError> for Error {
+    fn from(err: FixedValueWriteError) -> Error {
+        match err {
+            FixedValueWriteError(err) => Error(err)
+        }
+    }
+}
+
+pub fn write_value_ref<W>(wr: &mut W, val: &ValueRef) -> Result<(), Error>
+    where W: Write
+{
+    match val {
+        &ValueRef::Nil => try!(write_nil(wr)),
+        _ => unimplemented!(),
+    }
+
+    Ok(())
+}
