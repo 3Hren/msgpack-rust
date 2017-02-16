72b70b021525a889525dc32d6a528c0b43dfe86f
Evgeny Safronov <division494@gmail.com>
2017-02-16 16:45:30 +0300

    refactor: drop codegen from tests
    It’s okay, since stable supports serde now.

--- rmp-serde-tests/tests/derive/mod.rs
+++ rmp-serde-tests/tests/derive/mod.rs
@@ -1,29 +1,3 @@
-#[cfg(feature = "with-syntex")]
-mod de {
-    include!(concat!(env!("OUT_DIR"), "/de.rs"));
-}
-
-#[cfg(feature = "with-syntex")]
-mod se {
-    include!(concat!(env!("OUT_DIR"), "/se.rs"));
-}
-
-#[cfg(feature = "with-syntex")]
-mod round {
-    include!(concat!(env!("OUT_DIR"), "/round.rs"));
-}
-
-#[cfg(not(feature = "with-syntex"))]
-mod de {
-    include!("de.in.rs");
-}
-
-#[cfg(not(feature = "with-syntex"))]
-mod se {
-    include!("se.in.rs");
-}
-
-#[cfg(not(feature = "with-syntex"))]
-mod round {
-    include!("round.in.rs");
-}
+mod de;
+mod round;
+mod se;
--- rmp-serde-tests/tests/lib.rs
+++ rmp-serde-tests/tests/lib.rs
@@ -1,12 +1,8 @@
-#![cfg_attr(not(feature = "with-syntex"), feature(proc_macro, plugin))]
-
 #[macro_use]
 extern crate serde;
-extern crate rmp;
-extern crate rmp_serde;
-
-#[cfg(not(feature = "with-syntex"))]
 #[macro_use]
 extern crate serde_derive;
+extern crate rmp;
+extern crate rmp_serde;
 
 mod derive;
